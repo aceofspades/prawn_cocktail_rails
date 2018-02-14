daa23ed723e7823e06ddcf72b99f38d3f8779681
Doug Puchalski <aceofspades@users.noreply.github.com>
2018-02-14 10:53:11 -0700

    Avoid frozen array exception

--- lib/prawn_cocktail_rails/railtie.rb
+++ lib/prawn_cocktail_rails/railtie.rb
@@ -3,7 +3,7 @@ require "rails"
 module PrawnCocktailRails
   class Railtie < Rails::Railtie
     initializer "prawn_cocktail.autoload", before: :set_autoload_paths do |app|
-      app.config.autoload_paths << "#{app.config.root}/app/documents/helpers"
+      app.config.autoload_paths += ["#{app.config.root}/app/documents/helpers"]
     end
 
     initializer "prawn_cocktail.set_template_root" do |app|
